effb47b4ebda436cfeb2f8c8bdb5679b73a9c128
Daniel <dpet20@student.aau.dk>
2021-06-24 20:28:57 +0200

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # scrummaster-discord
+
 Github action to determine a team's scrummasters and post it in the team discord.
+
+## parameters
+
+team should be a JSON string with an array of strings, with minimum 2 members.
+
+team: "["Octo", "Cat"]"
+
+webhook should be a discord webhook in the form of an URL.
+
+### examples
+
+team: "["Octo", "Cat"]"
+
+webhook: https://discord.com/api/webhooks/32192256368640/aagaxz2cWHb-RfO0iO3fXJKrvMUaY11dsupZHr9_vYCJXs7n5GSpEn
---
+++ index.js
@@ -0,0 +1,44 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+const Discord = require("discord.js");
+
+try {
+  const team = JSON.parse(core.getInput("team"));
+  const webhook = core.getInput("webhook").split("/");
+  const githubRunNumber = process.env.GITHUB_RUN_NUMBER;
+  const githubRunID = process.env.GITHUB_RUN_ID;
+  const scrumMasters = determineScrumMasters(
+    team,
+    githubRunNumber,
+    githubRunID
+  );
+  messageDiscord(webhook, scrumMasters);
+} catch (error) {
+  core.setFailed(error.message);
+}
+
+function determineScrumMasters(team, number, variance = 1) {
+  const scrumMasterOne = team.splice(number % team.length(), 1);
+  const scrumMasterTwo = team.splice((number + variance) % team.length());
+  let scrumMasters = scrumMasterOne.concat(scrumMasterTwo);
+  return scrumMasters;
+}
+
+function messageDiscord(webhook, scrumMasters) {
+  const webhookClient = new Discord.WebhookClient(
+    webhook[webhook.length() - 2],
+    webhook.length() - 1
+  );
+
+  const embed = new Discord.MessageEmbed()
+    .setTitle("New Scrum Masters for our team")
+    .setColor("#0099ff")
+    .addField("First", scrumMasters[0])
+    .addField("Second", scrumMasters[1]);
+
+  webhookClient.send("Webhook test", {
+    username: "Github Action",
+    avatarURL: "https://i.imgur.com/wSTFkRM.png",
+    embeds: [embed],
+  });
+}
